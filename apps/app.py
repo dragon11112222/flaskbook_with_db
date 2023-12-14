44065e9ef7481781a5e2272c2cb961516fda4d9d
dragon11112222 <seyongjung36@gmail.com>
2023-12-14 14:27:05 +0900

    초기 설정(.env파일 내용 수정)
    FLASK_APP=apps.app:create_app
    FLASK_DEBUG=1

---
+++ apps/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def creat_app():
+    app = Flask(__name__)  ## 플라스크 인스턴스(객체) 생성
+
+    from apps.crud import views as crud_views
+
+    app.register_blueprint(crud_views.crud, url_prefix = '/crud')
+
+    return app
